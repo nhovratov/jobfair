bf6ad39e1c9948b4de1d3316e6707268b425b58f
Benjamin Franzke <bfr@qbus.de>
2022-04-04 15:21:17 +0200

    [TASK] Adapt extbase controller registration for TYPO3 v11

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,24 +4,28 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Dan.' . 'jobfair',
-    'Pi1',
-    [
-        'Job' => 'list, latest, show, new, create, edit, update, confirmDelete, delete, newApplication, createApplication',
-    ],
-    // non-cacheable actions
-    [
-        'Job' => 'list, latest, show, new, create, edit, update, confirmDelete, delete, newApplication, createApplication',
-    ]
-);
+(function() {
+    $controller = (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 11 ? \Dan\Jobfair\Controller\JobController::class : 'Job';
 
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'apps-pagetree-folder-contains-jobs',
-    \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-    ['source' => 'EXT:jobfair/Resources/Public/Icons/folder.gif']
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Dan.' . 'jobfair',
+        'Pi1',
+        [
+            $controller => 'list, latest, show, new, create, edit, update, confirmDelete, delete, newApplication, createApplication',
+        ],
+        // non-cacheable actions
+        [
+            $controller => 'list, latest, show, new, create, edit, update, confirmDelete, delete, newApplication, createApplication',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Dan\Jobfair\Property\TypeConverter\UploadedFileReferenceConverter::class);
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Dan\Jobfair\Property\TypeConverter\ObjectStorageConverter::class);
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'apps-pagetree-folder-contains-jobs',
+        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        ['source' => 'EXT:jobfair/Resources/Public/Icons/folder.gif']
+    );
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Dan\Jobfair\Property\TypeConverter\UploadedFileReferenceConverter::class);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Dan\Jobfair\Property\TypeConverter\ObjectStorageConverter::class);
+})();
