bf99d678d6c97a2c8bb83bac1752ba41b9a4f81a
Nikita Hovratov <nikita.h@live.de>
2022-04-05 14:26:15 +0200

    [RELEASE] Release version 4.0.0
    TYPO3 v11 compatibility.
    Dropped TYPO3 v9 compatibility.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,25 +4,17 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Job Fair',
     'description' => 'Simple job market based on Extbase and Fluid. Basically works like dmmjobcontrol. There are list and detail views available. In addition, it is possible to set up an online application system. Furthermore, FE-Users can be enabled to add and edit jobs in the frontend, so to BE-Administration is required. Feeds (Rss091, Rss2, Atom) are also available',
     'category' => 'plugin',
-    'version' => '3.0.1',
+    'version' => '4.0.0',
     'state' => 'stable',
     'uploadfolder' => true,
     'author' => 'Dan',
     'author_email' => 'typo3dev@outlook.com',
     'author_company' => '',
-    'constraints' =>
-    [
-        'depends' =>
-        [
-            'extbase' => '9.5.0-11.5.99',
-            'fluid' => '9.5.0-11.5.99',
-            'typo3' => '9.5.0-11.5.99',
-        ],
-        'conflicts' =>
-        [
-        ],
-        'suggests' =>
-        [
+    'constraints' => [
+        'depends' => [
+            'extbase' => '10.4.0-11.5.99',
+            'fluid' => '10.4.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
     ],
 ];
