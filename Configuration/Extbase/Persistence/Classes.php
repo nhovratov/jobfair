a81c6e82f207526d4fdcf530189bba0207ec8d9c
Nikita Hovratov <nikita.h@live.de>
2022-06-27 18:30:46 +0200

    [BUGFIX] Migrate class mapping to Classes.php

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -11,4 +11,15 @@ return [
             ],
         ],
     ],
+
+    \Dan\Jobfair\Domain\Model\User::class => [
+        'tableName' => 'fe_users',
+        'recordType' => 'Tx_Extbase_Domain_Model_FrontendUser',
+    ],
+
+    \TYPO3\CMS\Extbase\Domain\Model\FrontendUser::class => [
+        'subclasses' => [
+            'Tx_Jobfair_User' => \Dan\Jobfair\Domain\Model\User::class,
+        ],
+    ],
 ];
