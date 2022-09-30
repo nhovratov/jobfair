0523325e2f2b652c20b8e1729da8f0b89c4640e8
Maryana Rozhankivska <mariroz@mr.lviv.ua>
2022-09-30 13:00:28 +0200

    fix of TCA config errors: l10n_parent foreign selects

--- Configuration/TCA/tx_jobfair_domain_model_education.php
+++ Configuration/TCA/tx_jobfair_domain_model_education.php
@@ -65,9 +65,9 @@ return [
                         0
                     ]
                 ],
-                'foreign_table' => 'tx_jobfair_domain_model_application',
+                'foreign_table' => 'tx_jobfair_domain_model_education',
                 // no sys_language_uid = -1 allowed explicitly!
-                'foreign_table_where' => 'AND tx_jobfair_domain_model_application.uid=###CURRENT_PID### AND tx_jobfair_domain_model_application.sys_language_uid = 0',
+                'foreign_table_where' => 'AND tx_jobfair_domain_model_education.uid=###CURRENT_PID### AND tx_jobfair_domain_model_education.sys_language_uid = 0',
                 'default' => 0
             ]
         ],
