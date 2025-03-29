7e8f65ee44d8f1ab1d27b3da4e1e9e5d5bb94d70
Nikita Hovratov <nikita.h@live.de>
2025-03-29 20:17:13 +0100

    Migrate icon registration

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+
+return [
+    'apps-pagetree-folder-contains-jobs' => [
+        'provider' => BitmapIconProvider::class,
+        'source' => 'EXT:jobfair/Resources/Public/Icons/folder.gif',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 
 use Dan\Jobfair\Controller\JobController;
-use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 if (!defined('TYPO3')) {
@@ -20,13 +17,6 @@ if (!defined('TYPO3')) {
         [
             JobController::class => 'list, latest, show, new, create, edit, update, confirmDelete, delete, newApplication, createApplication',
         ],
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-    );
-
-    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'apps-pagetree-folder-contains-jobs',
-        BitmapIconProvider::class,
-        ['source' => 'EXT:jobfair/Resources/Public/Icons/folder.gif']
+        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
     );
 })();
